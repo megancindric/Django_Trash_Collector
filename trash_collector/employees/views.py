c18e042716454b899911305ba7762fcef604851e
Megan Cindric <megancindric@gmail.com>
2021-09-17 11:50:49 -0500

    Added logic to filter & display today's pickups

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -3,6 +3,8 @@ from django.http.response import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 from django.apps import apps
+from datetime import date
+import calendar
 
 # Create your views here.
 
@@ -13,20 +15,36 @@ def index(request):
     try:
         # This line inside the 'try' will return the customer record of the logged-in user if one exists
         logged_in_employee = Employee.objects.get(user=user)
-        Customer = apps.get_model('customers.Customer')
+       
+
+
+    except:
+        return HttpResponseRedirect(reverse('employees:create'))
+
+    Customer = apps.get_model('customers.Customer')
+    all_customers = Customer.objects.all()
+    matching_customers = []
+    today = date.today()
+    today_string = calendar.day_name[today.weekday()]
+    for customer in all_customers:
+        is_suspended = customer.suspension_start < today and customer.suspension_end > today
+        if customer.has_extra_pickup and customer.extra_pickup_day == today:
+            matching_customers.append(customer)
+        elif customer.pickup_day == today_string and not is_suspended:
+        #Today's date needs to be > start and <
+            matching_customers.append(customer)
+
+        #filter(zip_code=logged_in_employee.zip_code)
         #Matches our zip
         #Today is customer's pickup day
         #Customer is not suspended
         #Today is customer's 1-time pickup
-
-    except:
-        # TODO: Redirect the user to a 'create' function to finish the registration process if no customer record found
-        return HttpResponseRedirect(reverse('employees:create'))
-
-    # It will be necessary while creating a Customer/Employee to assign request.user as the user foreign key
-    
+    context = {
+        'matching_customers': matching_customers
+    }
     print(user)
-    return render(request, 'employees/index.html')
+    print(matching_customers)
+    return render(request, 'employees/index.html', context)
 
 def create(request):
     if request.method == "POST":
